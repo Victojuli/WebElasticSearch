d7cdedd1cb5fd6f83cbfb155c29cb0fa367e1d55
Victojuli <Victojuli@gmail.com>
2018-12-26 00:12:23 +0200

    ----

--- WebElasticSearch/Startup.cs
+++ WebElasticSearch/Startup.cs
@@ -8,9 +8,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using WebElasticSearch.Data;
 using WebElasticSearch.Models;
 using WebElasticSearch.Services;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
 
 namespace WebElasticSearch
 {
@@ -19,6 +23,16 @@ namespace WebElasticSearch
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            // Create Serilog Elasticsearch logger
+            Log.Logger = new LoggerConfiguration()
+             .Enrich.FromLogContext()
+             .MinimumLevel.Debug()
+             .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:44315"))
+             {
+                 MinimumLogEventLevel = LogEventLevel.Verbose,
+                 AutoRegisterTemplate = true
+             })
+             .CreateLogger();
         }
 
         public IConfiguration Configuration { get; }
@@ -36,17 +50,23 @@ namespace WebElasticSearch
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
+            // This method gets called by the runtime. Use this method to add services to the container.
+            services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-                app.UseDatabaseErrorPage();
+                //app.UseBrowserLink();
+               // app.UseDatabaseErrorPage();
             }
             else
             {
@@ -55,14 +75,13 @@ namespace WebElasticSearch
 
             app.UseStaticFiles();
 
-            app.UseAuthentication();
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
